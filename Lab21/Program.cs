3b9deec9f6096c61c366fcf57cf78b58e18dae13
a.ryzhakov2011@yandex.ru <a.ryzhakov2011@yandex.ru>
2022-01-13 17:43:33 +0300

    Добавьте файлы проекта.

---
+++ Lab21/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab21
+{
+    internal class Program
+    {
+        const int a = 5;
+        const int b = 5;
+        Random random = new Random(10);
+        random
+        static int[,] Garden = new int[a,b] { };
+        static void Main(string[] args)
+        {
+
+        }
+        static void Gardener1()
+        {
+            while (true)
+            {
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
