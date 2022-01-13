9b1ce1b9bdf9044b8b53a71dfba31bca763b09a6
a.ryzhakov2011@yandex.ru <a.ryzhakov2011@yandex.ru>
2022-01-13 19:38:20 +0300

    Добавил решение

--- Lab21/Program.cs
+++ Lab21/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Lab21
@@ -10,20 +11,66 @@ namespace Lab21
     {
         const int a = 5;
         const int b = 5;
-        Random random = new Random(10);
-        random
-        static int[,] Garden = new int[a,b] { };
+        static int[,] Garden = new int[a, b];
         static void Main(string[] args)
         {
+            Random random = new Random();
+            for (int i = 0; i < a; i++)
+            {
+                for (int j = 0; j < b; j++)
+                {
+                    Garden[i, j] = random.Next(0, 10);
+                    Console.Write($"{Garden[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
 
+            ThreadStart threadStart = new ThreadStart(Gardener1);
+            Thread thread = new Thread(threadStart);
+            thread.Start();
+
+            Gardener2();
+
+            for (int i = 0; i < a; i++)
+            {
+                for (int j = 0; j < b; j++)
+                {
+                    Console.Write($"{Garden[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
         }
         static void Gardener1()
         {
-            while (true)
+            for (int i = 0; i < a; i++)
             {
-                Console.WriteLine();
+                for (int j = 0; j < b; j++)
+                {
+                    if (Garden[i, j] >= 0)
+                    {
+                        int time = Garden[i, j];
+                        Garden[i, j] = -1;
+                        Thread.Sleep(time);
+                    }
+                }
             }
         }
-
+        static void Gardener2()
+        {
+            for (int i = a - 1; i >= 0; i--)
+            {
+                for (int j = b - 1; j >= 0; j--)
+                {
+                    if (Garden[i, j] >= 0)
+                    {
+                        int time = Garden[i, j];
+                        Garden[i, j] = -2;
+                        Thread.Sleep(time);
+                    }
+                }
+            }
+        }
     }
 }
